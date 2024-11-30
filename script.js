7b0317fb051b2c50b41c75712cd06c9215ad3082
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2024-11-30 19:45:04 +0300

    Подправил логику, частично очистил от лишнего.

--- script.js
+++ script.js
@@ -5,89 +5,77 @@ const searchInputEl = document.querySelector('.field__input')
 const moreBtnEl = document.querySelector('.gallery__more-btn')
 const searchForm = document.querySelector('.gallery__field')
 
-moreBtnEl.style.display = 'none'
 
-let ulEl = document.createElement('ul')
-ulEl.classList.add('preview-list')
+let ulEl; // будущий элемент списка в который будут добавляться элементы созданные на основе данных из объекта, объявлен глобально чтобы впоследствии очищать
 
-function createElements(dataObj){
+let skipStep = 0; // счетчик шага пагинации, т.е. то, сколько при последующем запросе пропустить элементов с начала
+const limitPerPage = 9; // лимит отображаемых на "страницу" гифок
 
-    previewEl.append(ulEl)
+moreBtnEl.style.display = 'none' // начальный дисплей кнопки "показать больше"
+
+
+function createElements(dataObj){ // функция создающая элементы
+
+    if(!document.querySelector('.preview-list')){
+        ulEl = document.createElement('ul') // создаем элемент ul в который будут добавляться элементы созданные на основе данных из объекта
+        ulEl.classList.add('preview-list') // добавляем класс для стилей
+
+        previewEl.append(ulEl) // добавляем в контейнер созданный раннее ul
+    }
 
     let arr = dataObj.data; // получаем массив объектов из объекта data
 
     arr.forEach(obj => {
-        let liEl = document.createElement('li')
-        liEl.classList.add('preview__item')
-        // поулчаем каждый из url отдельного объекта который будет разным на каждой итерации
+        let liEl = document.createElement('li') // создаем элемент списка для организации элементов изображений
+        liEl.classList.add('preview__item') // добавляем класс для стилей
+        // получаем каждый из url каждого отдельного объекта и заполняем шаблон разметки
         liEl.innerHTML = `
             <a href="${obj.images.original.url}" class="preview__link" target="_blank">
                 <img src="${obj.images.original.url}" alt="${obj.images.original.url}" class="preview__img"> 
             </a>
         `
-        ulEl.append(liEl)
-
-        console.log(obj.images.original.url);
+        ulEl.append(liEl) // добавляем на каждой итерации li в ul
     });
 }
 
+function getData(endpoint, searchQuery, skipStep = 0){ // функция для отправки сетевых запросов
 
-function getData(endpoint, searchQuery){
+    let url; // задаем переменную для url-адреса...
 
-    if(endpoint != 'search'){
-
-        fetch((`http://api.giphy.com/v1/gifs/${endpoint}?api_key=11T5UhJN8ROnL1ZQbnHDUCdIDDqBwvJh&limit=9`)) // отправляем запрос к выбранному эндпоинту
-        .then(response => { // получаем ответ от сервера
-            if(!response.ok){ // если не получаем или получаем с ошибкой, то кидаем новую ошибку ответа сети
-                throw new Error('Network response gone wrong...')
-            }
-
-            return response.json() // в случае получения успешного результата возвращаем преобразованный из JSON в JS объект
-        })
-        .then(dataObj => { // получаем JS объект с данными которые можно использовать
-            if(dataObj && dataObj.data){ // если получаем объект и если в объекте есть нужное свойство, то
-                console.log(dataObj);
-
-                createElements(dataObj) // вызываем функцию создания элементов передавая аргументом объект с необходимыми данными
-            } else {
-                throw new Error('No received data from the source...') // если не получаем иили получаем с ошибкой, то кидаем ошибку о том что данные не получены
-            }
-        })
-        .catch(error => {
-            console.log('Network Error', error); // ловим и выводим ошибку
-        })
-
+    if(endpoint != 'search'){ // ...и в зависимости от условий структура запроса изменяется
+        url = `http://api.giphy.com/v1/gifs/${endpoint}?api_key=11T5UhJN8ROnL1ZQbnHDUCdIDDqBwvJh&limit=${limitPerPage}&offset=${skipStep}`;
     } else {
-
-        fetch((`http://api.giphy.com/v1/gifs/${endpoint}?q=${searchQuery}&api_key=11T5UhJN8ROnL1ZQbnHDUCdIDDqBwvJh&limit=9`)) // отправляем запрос полученный из поля ввода к выбранному эндпоинту
-        .then(response => { // получаем ответ от сервера
-            if(!response.ok){ // если не получаем или получаем с ошибкой, то кидаем новую ошибку ответа сети
-                throw new Error('Network response gone wrong...')
-            }
-
-            return response.json() // в случае получения успешного результата возвращаем преобразованный из JSON в JS объект
-        })
-        .then(dataObj => { // получаем JS объект с данными которые можно использовать
-            if(dataObj && dataObj.data){ // если получаем объект и если в объекте есть нужное свойство, то
-                console.log(dataObj);
-
-                createElements(dataObj) // вызываем функцию создания элементов передавая аргументом объект с необходимыми данными
-            } else {
-                throw new Error('No received data from the source...') // если не получаем иили получаем с ошибкой, то кидаем ошибку о том что данные не получены
-            }
-        })
-        .catch(error => {
-            console.log('Network Error', error); // ловим и выводим ошибку
-        })
-
+        url = `http://api.giphy.com/v1/gifs/${endpoint}?q=${searchQuery}&api_key=11T5UhJN8ROnL1ZQbnHDUCdIDDqBwvJh&limit=${limitPerPage}&offset=${skipStep}`;
     }
-}
 
+    fetch((url)) // отправляем запрос к выбранному эндпоинту
+    .then(response => { // получаем ответ от сервера
+        if(!response.ok){ // если не получаем или получаем с ошибкой, то кидаем новую ошибку ответа сети
+            throw new Error('Network response gone wrong...')
+        }
+
+        return response.json() // в случае получения успешного результата возвращаем преобразованный из JSON в JS объект
+    })
+    .then(dataObj => { // получаем JS объект с данными которые можно использовать
+        if(dataObj && dataObj.data){ // если получаем объект и если в объекте есть нужное свойство, то
+            console.log(dataObj);
+            console.log('Data received');
+
+            createElements(dataObj) // вызываем функцию создания элементов передавая аргументом объект с необходимыми данными
+        } else {
+            throw new Error('No received data from the source...') // если не получаем иили получаем с ошибкой, то кидаем ошибку о том что данные не получены
+        }
+    })
+    .catch(error => {
+        console.log('Network Error', error); // ловим и выводим ошибку
+    })
+}
 
 function clearResult(){
-    ulEl.remove()
+    if(ulEl){
+        ulEl.remove()
+    }
 }
-
 
 linksEls.forEach(linkEl => {
     linkEl.addEventListener('click', e => { // по нажатию на элемент раздела...
@@ -96,29 +84,58 @@ linksEls.forEach(linkEl => {
             removable.classList.remove('active')
         })
 
-        e.target.classList.add('active') // ...и добавляет его только нажатому 
+        e.target.classList.add('active') // ...и добавляет его только цели события
 
-        if(linkEl.id != 'search'){
+        if(document.querySelector('.preview-list')){ // если при переключении вкладки элемент присутствует на странице, то удалить его
+            clearResult()
+        }
+
+        if (linkEl.id === 'trending' && linkEl.classList.contains('active')) {
             searchForm.style.display = 'none'
+            moreBtnEl.style.display = 'block'
             getData(linkEl.id)
-        } else{
+        } else if (linkEl.id === 'random') {
+            moreBtnEl.style.display = 'none'
+        } else {
             searchForm.style.display = 'flex'
+            moreBtnEl.style.display = 'none'
         }
     })
 })
-
 
 searchBtnsEls.forEach(fieldBtn => {
     fieldBtn.addEventListener('click', e => {
         e.preventDefault()
 
-        if(e.target.classList.contains('btn--search') || e.target.closest('.btn--search')){
-            let searchValue = searchInputEl.value
-            getData('search', searchValue)
-            moreBtnEl.style.display = 'block'
-        } else {
-            searchInputEl.value = ''
-            clearResult()
+        if(searchInputEl.value){
+            if(e.target.classList.contains('btn--search') || e.target.closest('.btn--search')){
+                let searchValue = searchInputEl.value
+                skipStep = 0
+                getData('search', searchValue, skipStep)
+                moreBtnEl.style.display = 'block'
+                clearResult()
+                // searchInputEl.value = ''
+            } else {
+                searchInputEl.value = ''
+                moreBtnEl.style.display = 'none'
+                clearResult()
+            }
         }
     })
+})
+
+moreBtnEl.addEventListener('click', e => {
+    if(e.target.classList.contains('btn--more') || e.target.closest('.btn--more')){
+
+        skipStep += limitPerPage // при каждом нажатии увеличивать шаг пропуска для получения следующих значений
+
+        let activeLink = document.querySelector('.gallery__link.active')
+
+        if(activeLink){
+            let endpoint = activeLink.id;
+            let searchQuery = searchInputEl.value;
+
+            getData(endpoint != 'search' ? endpoint : 'search', searchQuery, skipStep)
+        }
+    }
 })
